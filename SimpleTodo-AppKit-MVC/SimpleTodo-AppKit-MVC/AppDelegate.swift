81536793c2c55dd63e1454d9d8c9b0218327dad3
drypot <drypot@gmail.com>
2025-02-17 19:31:05 +0900

    add basic AppDelegate functions

--- SimpleTodo-AppKit-MVC/SimpleTodo-AppKit-MVC/AppDelegate.swift
+++ SimpleTodo-AppKit-MVC/SimpleTodo-AppKit-MVC/AppDelegate.swift
@@ -10,21 +10,52 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+    var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        setupWindow()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        saveWindowPosition()
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
+    func setupWindow() {
+        window = NSWindow(
+            contentRect: .zero,
+            styleMask: [.titled, .closable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+
+        window.title = "AppKit Demo"
+//        window.contentViewController = NavigatorController()
+        window.layoutIfNeeded()
+        restoreWindowPosition()
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    private func saveWindowPosition() {
+        let frameString = NSStringFromRect(window.frame)
+        UserDefaults.standard.set(frameString, forKey: "MainWindowFrame")
+    }
+
+    private func restoreWindowPosition() {
+        if let frameString = UserDefaults.standard.string(forKey: "MainWindowFrame") {
+            let frame = NSRectFromString(frameString)
+            window.setFrame(frame, display: true)
+        } else {
+            window.center()
+        }
+    }
+    
 }
 
